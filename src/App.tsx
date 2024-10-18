3b149f6938b58ef895215e6877d8d872ee02a6b6
Lee <dlwogml234@gmail.com>
2024-10-18 20:17:03 +0900

    초기배포

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import mainImage from "./assets/images/main.png";
+function App() {
+  return (
+    <>
+      <div>
+        <img src={mainImage} />
+      </div>
+    </>
+  );
+}
+
+export default App;
